950ae20b8c3b8a5c1e071dad8f8f0d1b2b19bb38
Beverly Liu <bevliu@gmail.com>
2019-05-22 21:00:21 -0700

    ITestListener

---
+++ src/webtest/TestNGListerner.java
@@ -0,0 +1,51 @@
+package webtest;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestNGListerner implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		System.out.println("Test case started and details are" + result.getName());
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test cases success and details are" + result.getName());
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test cases failed and details are" + result.getName());
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test cases skipped and details are" + result.getName());
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
